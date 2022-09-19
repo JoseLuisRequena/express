b550d933dae480dea3fc87f4846db9ab68e78996
unknown <jlrequenaetcheverria@gmail.com>
2022-09-19 09:11:05 +0200

    login

---
+++ auth/auth.js
@@ -0,0 +1,49 @@
+const passport = require('passport');
+const localStrategy = require('passport-local').Strategy;
+const JWTstrategy = require('passport-jwt').Strategy;
+const ExtractJWT = require('passport-jwt').ExtractJwt;
+
+
+const user = {
+  username: 'Admin',
+  password: 'Admin',
+};
+
+
+passport.use(
+  'login',
+  new localStrategy(
+    {
+      usernameField: 'username',
+      passwordField: 'password',
+    },
+    async (username, password, done) => {
+      try {
+        if (username === user.username && password === user.password) {
+          return done(null, user, { message: 'Logged in Successfully' });
+        }
+        return done(null, false, {
+          message: 'User not found or Wrong Password',
+        });
+      } catch (error) {
+        return done(error);
+      }
+    }
+  )
+);
+
+passport.use(
+  new JWTstrategy(
+    {
+      secretOrKey: 'TOP_SECRET',
+      jwtFromRequest: ExtractJWT.fromUrlQueryParameter('secret_token')
+    },
+    async (token, done) => {
+      try {
+        return done(null, token.user);
+      } catch (error) {
+        done(error);
+      }
+    }
+  )
+);
--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -1,7 +1,38 @@
+const passport = require("passport");
+const jwt = require("jsonwebtoken");
 const controller = {};
 
-controller.index = function(req, res, next) {
-    res.render('index', { title: 'Jose Luis' });
-}
-
+//controller.index = function(req, res, next) {
+//    res.render('index', { title: 'Jose Luis' });
+//}
+controller.login = (
+    async (req, res, next) => {
+        passport.authenticate(
+            'login',
+            async (err, user, info) => {
+                try {
+                    if (err || !user) {
+                        const error = new Error('An error occurred.');
+                        
+                        return next(error);
+                    }
+                  
+                    req.login( user, { session: false }, 
+                        async (error) => {
+                            if (error) return next(error);
+                            
+                            const body = { _id: user._id, email: user.email };
+                            const token = jwt.sign({ user: body }, 'TOP_SECRET');
+                            
+                            return res.json({ token });
+                        }
+                    );
+                } catch (error) {
+                  return next(error);
+                }
+            }
+        )(req, res, next);
+    }
+);
+  
 module.exports = controller
