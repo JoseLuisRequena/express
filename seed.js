53de61dec8bdd26a07df2484c444bb9800277000
unknown <jlrequenaetcheverria@gmail.com>
2022-09-29 16:32:33 +0200

    create data whit seed

--- seed.js
+++ seed.js
@@ -1,114 +1,98 @@
-//conectar base de MySQL
-const  global = require('./env');
-
 const { faker } = require("@faker-js/faker");
 const bcrypt = require("bcrypt");
+const {Room, User, Contact, Booking} = require("./schema")
 
-const mysql = require('mysql');
-const connection = mysql.createConnection({
-  host: 'localhost',
-  database: 'miranda_db',
-  user: global.db_user,
-  password: global.db_password
-});
+require('./connectionDB')
 
-connection.connect(function(error){
-  if(error){
-    throw error;
-  }else{
-    console.log('conexion exitosa');
-  }
-});
+async function saveItem(item) {
+    await item.save();
+    console.log(item);
+}
 
-// Rooms
-const sqlRooms = "INSERT INTO rooms (room_number, bed_type, description, offer, price, cancellation, amenities) VALUES (?)";
-for (let i = 1; i <= 10; i++) {
-    let values = [
-        i,
-        faker.helpers.arrayElement([
-            "single_bed",
-            "double_bed",
-            "double_superior",
-            "suite",
+for (let i = 0; i < 10; i++) {
+    let newUser = new User ({
+        user_name: faker.name.firstName() + faker.name.lastName(),
+        user_email: faker.internet.email(),
+        user_phone: faker.phone.number("###-###-###"),
+        start_date: faker.date.past(),
+        occupation: faker.helpers.arrayElement([
+            "administrative manager",
+            "administrative staff",
+            "administrative staff",
+            "administrative staff",
+            "administrative staff",
+            "administrative staff",
+            "administrative staff",
+            "administrative staff",
+            "administrative staff",
+            "administrative staff"
         ]),
-        faker.lorem.sentence(10),
-        faker.helpers.arrayElement([0, 1]),
-        faker.finance.amount(50, 100, 0),
-        faker.lorem.sentence(10),
-        ["TV", "WIFI", "BATHROOM-KIT"]
-            .concat( faker.helpers.arrayElements(["JACUZZI", "HAIR-DRYER", "MINIBAR"], 2))
-            .join(" "),
-    ];
-
-    connection.query(sqlRooms, [values], function (err, result) {
-        if (err) throw err;
-        console.log("Number of records inserted: " + result.affectedRows);
+        status: faker.helpers.arrayElement([0, 1]),
+        user_image: faker.image.avatar(),
+        password: bcrypt.hashSync(faker.internet.password(), 5)
     });
 
-    // Room_images
-    for (let j = 1; j <= 5; j++) {
-        const sqlImages = "INSERT INTO rooms_images (room_id, url_image) VALUES (?)";
-        let values = [i, faker.image.imageUrl("", "", "", true)];
-        connection.query(sqlImages, [values], function (err, result) {
-            if (err) throw err;
-            console.log("Number of records inserted: " + result.affectedRows);
-        });
-    }
-}
+    saveItem(newUser)
+};
 
-const sqlBookings = "INSERT INTO bookings (guest_name, order_date, checkin, checkout, special_request, room_id, status) VALUES (?)";
 for (let i = 0; i < 10; i++) {
-    let values = [
-        faker.name.firstName() + " " + faker.name.lastName(),
-        faker.date.past(),
-        faker.date.future(),
-        faker.date.future(),
-        faker.hacker.phrase(),
-        Math.floor(Math.random() * (10 - 1) + 1),
-        faker.helpers.arrayElement(["checkIn", "checkOut", "inProgress"]),
-    ];
-    connection.query(sqlBookings, [values], function (err, result) {
-        if (err) throw err;
-        console.log("Number of records inserted: " + result.affectedRows);
+    let newContact = new Contact ({
+        contact_name: faker.name.firstName() + " " + faker.name.lastName(),
+        contact_email: faker.internet.email(),
+        contact_phone: faker.phone.number("###-###-###"),
+        contact_date: faker.date.past(),
+        subject: faker.hacker.phrase(),
+        comment: faker.lorem.sentence(),
+        viewed: faker.helpers.arrayElement([0, 1]),
+        archived: faker.helpers.arrayElement([0, 1]),
     });
-}
 
-// Contacts
-const sqlContacts = "INSERT INTO contacts (contact_name, contact_email, contact_phone, contact_date, subject, comment, viewed, archived) VALUES (?)";
+    saveItem(newContact)
+};
+
+const rooms = [];
 for (let i = 0; i < 10; i++) {
-    let values = [
-        faker.name.firstName() + " " + faker.name.lastName(),
-        faker.internet.email(),
-        faker.phone.number("###-###-###"),
-        faker.date.past(),
-        faker.hacker.phrase(),
-        faker.lorem.sentence(),
-        faker.helpers.arrayElement([0, 1]),
-        faker.helpers.arrayElement([0, 1]),
-    ];
-    connection.query(sqlContacts, [values], function (err, result) {
-        if (err) throw err;
-        console.log("Number of records inserted: " + result.affectedRows);
+    let newRoom = new Room ({
+      room_number: i,
+      bed_type: faker.helpers.arrayElement([
+        "single_bed",
+        "double_bed",
+        "double_superior",
+        "suite",
+      ]),
+      description: faker.lorem.sentence(10),
+      offer: faker.helpers.arrayElement([0, 1]),
+      price: faker.finance.amount(50, 100, 0),
+      discount: faker.finance.amount(0, 15, 0),
+      cancellation: faker.lorem.sentence(10),
+      amenities: ["TV", "WIFI", "BATHROOM-KIT"]
+        .concat(
+          faker.helpers.arrayElements(["JACUZZI", "HAIR-DRYER", "MINIBAR"], 1)
+        )
+        .join(" "),
+      images: [
+        faker.image.imageUrl("", "", "", true),
+        faker.image.imageUrl("", "", "", true),
+        faker.image.imageUrl("", "", "", true),
+        faker.image.imageUrl("", "", "", true),
+        faker.image.imageUrl("", "", "", true),
+      ],
     });
+  
+    saveItem(newRoom);
+    rooms.push(newRoom);
 }
 
-// Users
-const sqlUsers = "INSERT INTO users (user_name, user_email, user_phone, start_date, occupation, status, user_image, password) VALUES (?)";
 for (let i = 0; i < 10; i++) {
-    let values = [
-        faker.name.firstName() + faker.name.lastName(),
-        faker.internet.email(),
-        faker.phone.number("###-###-###"),
-        faker.date.past(),
-        faker.helpers.arrayElement(['administrative manager','administrative staff']),
-        faker.helpers.arrayElement([0, 1]),
-        faker.image.avatar(),
-        bcrypt.hashSync(faker.internet.password(), 5)
-    ];
-    connection.query(sqlUsers, [values], function (err, result) {
-        if (err) throw err;
-        console.log("Number of records inserted: " + result.affectedRows);
+    let index = Math.floor(Math.random() * (10 - 1) + 1);
+    let newBooking = new Booking({
+        guest_name: faker.name.firstName() + " " + faker.name.lastName(),
+        order_date: faker.date.past(),
+        checkin: faker.date.future(),
+        checkout: faker.date.future(),
+        special_request: faker.hacker.phrase(),
+        status: faker.helpers.arrayElement(["checkIn", "checkOut", "inProgress"]),
+        room_id: rooms[index]._id,
     });
+    saveItem(newBooking);
 }
-
-connection.end();
